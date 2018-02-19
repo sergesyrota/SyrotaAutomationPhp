d5189d77ef9632e201eebd29f9e23604621978c5
Sergey Syrota <serge.syrota@gmail.com>
2018-02-19 10:01:15 -0600

    Refactored library not to rely on php-gearman library, but rather use all-php implementation (due to non-trivial install requirement on PHP 7)

--- src/SyrotaAutomation/Gearman.php
+++ src/SyrotaAutomation/Gearman.php
@@ -16,7 +16,7 @@ class Gearman
   private $task;
 
   public function __construct($task, $host = 'localhost', $port = 4730) {
-    $this->client = new \GearmanClient();
+    $this->client = new \MHlavac\Gearman\Client();
     $this->client->addServer($host, $port);
     $this->task = $task;
   }
@@ -26,17 +26,15 @@ class Gearman
     return $this;
   }
 
+  /**
+   * Sends a command using SyrotaAutomation protocol
+   *
+   * @param string $address Device address
+   * @param string $command Command to send to device (including any arguments, for things like set commands)
+   */
   public function command($address, $command) {
     $res = $this->client->doNormal($this->task, chr(2) . $address . '>'. $command . "\n");
-    switch($this->client->returnCode()) {
-      case GEARMAN_SUCCESS:
-        continue;
-      case GEARMAN_TIMEOUT:
-        throw new Exception("Gearman request timed out");
-        break;
-      default:
-        throw new Exception("Gearman error #{$this->getErrno()}: {$this->client->error()}");
-    }
+    // This pattern represents response pattern slave device is expected to send
     preg_match('%\x02>(.*)\n%', $res, $matches);
     if (!isset($matches[1])) {
       throw new Exception("RS485 response does not match pattern: {$res}");
